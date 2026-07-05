c2bf55ef8e7ae9015bb9b13046fe00148251a53e
Muhammed Suhail <muhammedsuhailop@gmail.com>
2026-07-05 12:18:20 +0530

    fix:google auth fix for render

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(session({
 }))
 app.use(flash());
 
+app.set("trust proxy", 1);
 app.use(passport.initialize());
 app.use(passport.session());
 
--- config/passport.js
+++ config/passport.js
@@ -8,30 +8,34 @@ const callbackURL =
         ? process.env.GOOGLE_CALLBACK_PROD
         : process.env.GOOGLE_CALLBACK_LOCAL;
 
-passport.use(new googleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: callbackURL,
-},
+passport.use(
+  new googleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: callbackURL,
+      proxy: true,
+    },
     async (accessToken, refreshToken, profile, done) => {
-        try {
-            let user = await User.findOne({ googleId: profile.id });
-            if (user) {
-                return done(null, user)
-            } else {
-                user = new User({
-                    name: profile.displayName,
-                    email: profile.emails[0].value,
-                    googleId: profile.id
-                });
-                await user.save();
-                return done(null, user)
-            }
-        } catch (error) {
-            return done(error, null);
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          return done(null, user);
+        } else {
+          user = new User({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id,
+          });
+          await user.save();
+          return done(null, user);
         }
-    }
-))
+      } catch (error) {
+        return done(error, null);
+      }
+    },
+  ),
+);
 
 passport.serializeUser((user, done) => {
     done(null, user.id)
